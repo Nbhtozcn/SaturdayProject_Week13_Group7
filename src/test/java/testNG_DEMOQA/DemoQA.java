c0ac4f872ede5eec5de46e80b0ac89ae1c5f7e6b
Nbhtozcn <nebahatasam@hotmail.com>
2023-03-01 22:11:02 -0500

    Initial commit

---
+++ src/test/java/testNG_DEMOQA/DemoQA.java
@@ -0,0 +1,80 @@
+package testNG_DEMOQA;
+
+import org.testng.annotations.Test;
+
+public class DemoQA {
+    @Test
+    void testID1(){
+
+    }
+    @Test
+    void testID2(){
+
+    }
+    @Test
+    void testID3(){
+
+    }
+    @Test
+    void testID4(){
+
+    }
+    @Test
+    void testID5(){
+
+    }
+    @Test
+    void testID6(){
+
+    }
+    @Test
+    void testID7(){
+
+    }
+    @Test
+    void testID8(){
+
+    }
+    @Test
+    void testID9(){
+
+    }
+    @Test
+    void testID10(){
+
+    }
+    @Test
+    void testID11(){
+
+    }
+    @Test
+    void testID12(){
+
+    }
+    @Test
+    void testID13(){
+
+    }
+    @Test
+    void testID14(){
+
+    }
+    @Test
+    void testID15(){
+
+    }
+    @Test
+    void testID16(){
+
+    }
+    @Test
+    void testID17(){
+
+    }
+    @Test
+    void testID18(){
+
+    }
+
+
+}
